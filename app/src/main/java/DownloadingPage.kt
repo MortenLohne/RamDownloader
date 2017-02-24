471d52c8d7e7725b2573ed9599e21fc467c50133
Andre Dyrstad <dyrstad.a@gmail.com>
2017-02-24 23:02:48 +0100

    Added a kotlin file to downloadingPage

---
+++ app/src/main/java/DownloadingPage.kt
@@ -0,0 +1,14 @@
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.morten.ramdownloader.R
+
+/**
+ * Created by ady on 24/02/2017.
+ */
+class DownloadingPage : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
