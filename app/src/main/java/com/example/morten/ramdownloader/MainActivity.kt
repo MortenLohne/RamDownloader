dfd36c705bce99908b7cc1a84e25de1532399f42
Morten Lohne <lohnemorten@gmail.com>
2017-02-24 21:52:40 +0100

    1GB button now prints to sysout when clicked

--- app/src/main/java/com/example/morten/ramdownloader/MainActivity.kt
+++ app/src/main/java/com/example/morten/ramdownloader/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.morten.ramdownloader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get reference to download buttons
+        val downloadButtons = mutableMapOf(1 to findViewById(R.id.downloadButton1GB) as Button)
+        downloadButtons[1]?.setOnClickListener ({  println("Button clicked!") })
     }
 }
