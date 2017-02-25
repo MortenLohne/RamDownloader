9b6fa2e54eb72d9afec1a3fec0e1091b0a19dcf6
Andre Dyrstad <dyrstad.a@gmail.com>
2017-02-25 14:35:24 +0100

    Added a new frontpage

--- app/src/main/java/com/example/morten/ramdownloader/DownloadingPage.kt
+++ app/src/main/java/com/example/morten/ramdownloader/DownloadingPage.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import com.example.morten.ramdownloader.R
 
 /**
@@ -22,6 +24,12 @@ class DownloadingPage : AppCompatActivity()  {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        setContentView(R.layout.activity_download)
+
+        val text1 = findViewById(R.id.downloadingText) as TextView
+
 
         // For some reason intent.extras.getInt("size") works but not intent.extras.getInt("size")
         var GBsToDownload : Int = intent.extras.getInt("size")
@@ -43,6 +51,10 @@ class DownloadingPage : AppCompatActivity()  {
         Thread(Runnable {
             while (status < 100.0) {
                 status += 3.0 /  GBsToDownload.toDouble()
+
+                if (status > 30){
+
+                }
 
                 Thread.sleep(200)
                 // Update the progress bar
---
+++ app/src/main/java/com/example/morten/ramdownloader/Trail.kt
@@ -0,0 +1,28 @@
+package com.example.morten.ramdownloader
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+/**
+ * Created by ady on 25/02/2017.
+ */
+
+class Trail : AppCompatActivity() {
+
+   override fun onCreate(savedInstanceState: Bundle?){
+
+       super.onCreate(savedInstanceState)
+       setContentView(R.layout.activity_trail)
+
+       var button = findViewById(R.id.closeButton)
+
+       button.setOnClickListener {
+           var intent = Intent(this,MainActivity::class.java)
+           startActivityForResult(intent,0)
+       }
+
+   }
+
+}
